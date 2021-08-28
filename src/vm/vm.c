c342de73c1cd020c62b8bf7a04812a4d9d659b4f
Lloyd Dilley <ldilley@users.noreply.github.com>
2021-08-28 06:17:07 -0500

    Use llabs() instead of abs()

--- src/vm/vm.c
+++ src/vm/vm.c
@@ -76,9 +76,9 @@ void print_registers()
     if(i == REGISTER_AMOUNT / 2)
       puts("");
     if(i < 10)
-      printf("R%u: %c0x%08lX   ", i, "+-"[(BigNum)vm.registers[i] < 0], (BigNum)abs(vm.registers[i]));
+      printf("R%u: %c0x%08lX   ", i, "+-"[vm.registers[i] < 0], (BigNum)llabs(vm.registers[i]));
     else
-      printf("R%u: %c0x%08lX  ", i, "+-"[(BigNum)vm.registers[i] < 0], (BigNum)abs(vm.registers[i]));
+      printf("R%u: %c0x%08lX  ", i, "+-"[vm.registers[i] < 0], (BigNum)llabs(vm.registers[i]));
   }
   puts("");
   return;
